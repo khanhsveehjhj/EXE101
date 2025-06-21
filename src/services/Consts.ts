f152905f8c1427caf663e0d9c717a23ee6dc1fbc
khanhsveehjhj <caophuongkhanhvy@gmail.com>
2025-06-21 22:23:36 +0700

    fix case sensitive

--- src/services/Consts.ts
+++ src/services/Consts.ts
@@ -8,19 +8,19 @@ import urology from '@/assets/Images/Icons/Urology.png';
 import medicine from '@/assets/Images/Icons/Medicine.png';
 import seeMoreImage from '@/assets/Images/Icons/seeMoreImage.png';
 
-import Banner1 from '@/assets/Images/Banner1.png';
-import Banner2 from '@/assets/Images/Banner2.png';
+import Banner1 from '@/assets/Images/banner1.png';
+import Banner2 from '@/assets/Images/banner2.png';
 
 import doc1 from '@/assets/Images/Doctors/doc1.png';
 import doc2 from '@/assets/Images/Doctors/doc2.png';
 import doc3 from '@/assets/Images/Doctors/doc3.png';
 import doc4 from '@/assets/Images/Doctors/doc4.png';
 
-import user1 from '@/assets/Images/users/user1.jpg';
-import user2 from '@/assets/Images/users/user2.jpg';
-import user3 from '@/assets/Images/users/user3.jpg';
+import user1 from '@/assets/Images/Users/user1.jpg';
+import user2 from '@/assets/Images/Users/user2.jpg';
+import user3 from '@/assets/Images/Users/user3.jpg';
 
-import logo from '@/assets/Images/logo.png';
+import logo from '@/assets/Images/Logo.png';
 
 export const links = ['Trang chủ', 'Bệnh viện', 'Dịch vụ', 'Đặt lịch', 'Bác sĩ', 'Đánh giá'];
 
